b415e541dae234a245187260f34115a07b096ed1
anilkumarmeena <anil98meena@gmail.com>
2019-06-13 19:05:04 +0530

    libraries

--- android/src/main/java/com/example/flutter_myfatoorah/FlutterMyfatoorahPlugin.java
+++ android/src/main/java/com/example/flutter_myfatoorah/FlutterMyfatoorahPlugin.java
@@ -5,18 +5,36 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import com.google.gson.Gson;
+import com.myfatoorah.sdk.MFSDKListener;
+import com.myfatoorah.sdk.model.invoice.InvoiceModel;
+import com.myfatoorah.sdk.model.transaction.TransactionResponseModel;
+import com.myfatoorah.sdk.utils.*;
+import com.myfatoorah.sdk.views.MFSDK;
+
+
 
 /** FlutterMyfatoorahPlugin */
 public class FlutterMyfatoorahPlugin implements MethodCallHandler {
   /** Plugin registration. */
+   static String BASE_URL = "https://apidemo.myfatoorah.com/";
+   static String EMAIL = "apiaccount@myfatoorah.com" ;// Merchant Email
+   static String PASSWORD = "api12345*";  // Merchant Password
+    String TAG = "PAYMENT";
+    String data ,can;
+
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_myfatoorah");
     channel.setMethodCallHandler(new FlutterMyfatoorahPlugin());
+    MFSDK.INSTANCE.init(BASE_URL, EMAIL, PASSWORD);
+
   }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
+
+    if (call.method.equals("payment")) {
+      
       result.success("Android " + android.os.Build.VERSION.RELEASE);
     } else {
       result.notImplemented();
